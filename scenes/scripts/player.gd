0dcd1fe1f5f74211d1280c0e17da3c3ec0ac1c9f
spdskatr <25078279+spdskatr@users.noreply.github.com>
2025-02-07 21:55:14 +0000

    add wall and player direction

--- scenes/scripts/player.gd
+++ scenes/scripts/player.gd
@@ -2,22 +2,16 @@ extends CharacterBody2D
 
 @export var speed: float = 1000.0
 
-func _physics_process(delta):
-	var direction = Vector2.ZERO
-	
-	if Input.is_action_pressed("ui_right"):
-		direction.x += 1
-	if Input.is_action_pressed("ui_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		direction.y += 1
-	if Input.is_action_pressed("ui_up"):
-		direction.y -= 1
-	
+func get_input():
+	var direction = Input.get_vector("left", "right", "up", "down")
 	if direction.length() > 0:
 		direction = direction.normalized()
-	
+		if not Input.is_action_pressed("shift"):
+			rotation = direction.angle()
 	velocity = direction * speed
+
+func _physics_process(delta):
+	get_input()
 	move_and_slide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
