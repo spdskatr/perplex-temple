0fa91643e23ae5b55578149fedeef214721cda99
jbachurski <kbachurski@gmail.com>
2025-02-08 16:54:51 +0000

    slides modulates vision and hearing

--- scenes/scripts/player.gd
+++ scenes/scripts/player.gd
@@ -5,6 +5,26 @@ extends CharacterBody2D
 @export var walk_speed : float = 200.0
 @export var light: PointLight2D
 
+func _ready() -> void:
+	var slider = get_node_or_null("../HUD/SliderBox/HSlider")
+	if slider:
+		slider.value_changed.connect(_on_slider_changed)
+
+func _on_slider_changed(value: float) -> void:
+	var slider = get_node("../HUD/SliderBox/HSlider")
+	var ratio = (slider.value - slider.min_value) / (slider.max_value - slider.min_value)
+	var cx = 51
+	var cy = 19
+	var rad = 15
+	var alpha = ratio * TAU/4
+	var dx = rad * cos(alpha)
+	var dy = rad * sin(alpha)
+	$EarLight.energy = (1 - ratio) * 1
+	$EyeArea/Occlude.occluder.polygon[0] = Vector2(cx + dx, cy - dy)
+	$EyeArea/Occlude.occluder.polygon[2] = Vector2(cx + dx, cy + dy)
+	$EyeArea/Collision.polygon[0] = Vector2(cx + dx, cy - dy)
+	$EyeArea/Collision.polygon[2] = Vector2(cx + dx, cy + dy)
+
 func get_input():
 	var direction = Input.get_vector("left", "right", "up", "down")
 	var magnitude = direction.length()
