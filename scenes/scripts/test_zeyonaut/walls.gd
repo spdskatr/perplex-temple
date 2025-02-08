2a3cbfed8597f41564598cc0c500c7237ce7650c
Aaron Yeoh Cruz <zeyonaut@gmail.com>
2025-02-08 14:55:37 +0000

    Test point and directional lights

---
+++ scenes/scripts/test_zeyonaut/walls.gd
@@ -0,0 +1,15 @@
+extends TileMapLayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var slider = get_node("/root/TestSlider/HUD/SliderBox/HSlider")
+	slider.value_changed.connect(_on_slider_changed)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_slider_changed(value: float) -> void:
+	var slider = get_node("/root/TestSlider/HUD/SliderBox/HSlider")
+	var ratio = (slider.value - slider.min_value) / (slider.max_value - slider.min_value)
+	material.set_shader_parameter("brightness", ratio)
