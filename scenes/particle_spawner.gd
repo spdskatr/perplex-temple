be0cd1cd665c499b2f08e39495f3c392844ec8cb
Aaron Yeoh Cruz <zeyonaut@gmail.com>
2025-02-08 17:00:30 +0000

    Add particle spawner

--- scenes/particle_spawner.gd
+++ scenes/particle_spawner.gd
@@ -1,18 +1,17 @@
 extends Node2D
 
-var interval : float = 1
-var offset : float = 0
+@export var particle_scene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	offset = Time.get_ticks_msec()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var time = Time.get_ticks_msec()
-	if (time > offset + interval):
-		offset += interval
-		if (time > offset + interval):
-			offset = time
-	else:
-		pass
+	pass
+
+func _on_particle_timer_timeout() -> void:
+	var particle = particle_scene.instantiate()
+	var particle_spawn_location = get_node(".")
+	particle.initialize(Color.YELLOW, 1, 10, 50)
+	add_child(particle)
