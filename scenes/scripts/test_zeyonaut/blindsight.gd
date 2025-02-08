4985d40080cc90f890ff8171850e5321e513c13e
Aaron Yeoh Cruz <zeyonaut@gmail.com>
2025-02-08 19:35:21 +0000

    Make particle layer invisible by default

---
+++ scenes/scripts/test_zeyonaut/blindsight.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var slider = get_node("/root/TestSlider/HUD/SliderBox/HSlider")
+	slider.value_changed.connect(_on_slider_changed)
+	material.set_shader_parameter("brightness", 0)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func _on_slider_changed(value: float) -> void:
+	var slider = get_node("/root/TestSlider/HUD/SliderBox/HSlider")
+	var ratio = (slider.value - slider.min_value) / (slider.max_value - slider.min_value)
+	material.set_shader_parameter("brightness", 1 - ratio)
