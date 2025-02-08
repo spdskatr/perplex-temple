44a3db5f27746b3666ec0eaf53e60c5d08de86d7
Rokcas <rokas0090@gmail.com>
2025-02-08 12:21:02 +0000

    Add an element-based light to player

--- scenes/scripts/element.gd
+++ scenes/scripts/element.gd
@@ -1,12 +1,16 @@
 class_name Element
 extends Node
 
-enum TYPE {DEFAULT, STONE}
+enum TYPE {DEFAULT, STONE, LIGHT, WOOD, FIRE, ICE}
 
 static func can_move(type) -> bool:
 	match type:
-		TYPE.DEFAULT:
-			return true
 		TYPE.STONE:
 			return false
 	return true
+
+static func glows(type) -> bool:
+	match type:
+		TYPE.LIGHT:
+			return true
+	return false
--- scenes/scripts/player.gd
+++ scenes/scripts/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @export var element: Element.TYPE
 @export var speed: float = 500.0
+@export var light: PointLight2D
 
 func get_input():
 	var direction = Input.get_vector("left", "right", "up", "down")
@@ -20,6 +21,10 @@ func _physics_process(_delta):
 	get_input()
 	move_and_slide()
 
+func update_light():
+	# Yeah, this is a hack
+	light.energy = Element.glows(element)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	update_light()
