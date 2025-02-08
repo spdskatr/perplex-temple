6f7736994f314d5097e8aac0605e0a1c7d1a3785
Rokcas <rokas0090@gmail.com>
2025-02-08 20:20:53 +0000

    Add a working door to level0

--- scenes/levels/level_0.gd
+++ scenes/levels/level_0.gd
@@ -3,6 +3,6 @@ extends Node2D
 func change_scene_to_main():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
-func _on_exit_entered(body: Node2D) -> void:
+func _on_door_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
 		change_scene_to_main.call_deferred()
