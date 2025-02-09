76a4e3344eaea7c52616a9850d5fd16d3d1987d3
jbachurski <kbachurski@gmail.com>
2025-02-09 15:00:02 +0000

    make death zone take longer to kill

--- scenes/objects/death_zone.gd
+++ scenes/objects/death_zone.gd
@@ -1,7 +1,14 @@
 extends Area2D
 
+const time_to_die: float = 0.15
+var timeout: float = 0.0
+
 func _process(_delta):
 	for body in get_overlapping_bodies():
 		if body is Player:
 			if !body.is_wet():
-				body.die()
+				timeout += _delta
+				if timeout > time_to_die:
+					body.die()
+			else:
+				timeout = 0
