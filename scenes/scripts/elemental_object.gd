57226d7501e5d823ddfe1d4c801d36b377759a18
jbachurski <kbachurski@gmail.com>
2025-02-09 15:25:01 +0000

    optimize raycasts

--- scenes/scripts/elemental_object.gd
+++ scenes/scripts/elemental_object.gd
@@ -3,19 +3,25 @@ extends Node2D
 @export var element: Element.TYPE
 @onready var player = Global.player
 @export var visibility: VisibleOnScreenNotifier2D
-var timeout: float = 0
+var apply_timeout: float = 0
 
 func _ready() -> void:
 	$RayCast2D.add_exception(player)
 
 func _process(_delta: float) -> void:
 	var heart = player.get_node("Heart")
-	$RayCast2D.target_position = $RayCast2D.to_local(player.global_position)
-	var t = $RayCast2D.get_collider()
-	if t == heart and visibility.is_on_screen():
-		timeout = 0
-		player.visible_objects[get_instance_id()] = element
+	if visibility.is_on_screen():
+		$RayCast2D.target_position = $RayCast2D.to_local(player.global_position)
+		var t = $RayCast2D.get_collider()
+		if t == heart:
+			apply_timeout = 0
+			player.visible_objects[get_instance_id()] = element
+		else:
+			_deapply_tick(_delta)
 	else:
-		timeout += _delta
-		if timeout > 0.1:
-			player.visible_objects.erase(get_instance_id())
+		_deapply_tick(_delta)
+
+func _deapply_tick(_delta):
+	apply_timeout += _delta
+	if apply_timeout > 0.1:
+		player.visible_objects.erase(get_instance_id())
