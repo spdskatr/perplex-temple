5d3e1c3c15d509359c2e7ee060cdf40267fa9430
jbachurski <kbachurski@gmail.com>
2025-02-09 15:05:13 +0000

    trying to fix status flickering

--- scenes/scripts/elemental_object.gd
+++ scenes/scripts/elemental_object.gd
@@ -3,15 +3,19 @@ extends Node2D
 @export var element: Element.TYPE
 @onready var player = Global.player
 @export var visibility: VisibleOnScreenNotifier2D
+var timeout: float = 0
+
+func _ready() -> void:
+	$RayCast2D.add_exception(player)
 
 func _process(_delta: float) -> void:
 	var heart = player.get_node("Heart")
 	$RayCast2D.target_position = $RayCast2D.to_local(player.global_position)
-	$RayCast2D.add_exception(player)
 	var t = $RayCast2D.get_collider()
 	if t == heart and visibility.is_on_screen():
+		timeout = 0
 		player.visible_objects[get_instance_id()] = element
 	else:
-		if player.visible_objects.has(get_instance_id()):
-			await get_tree().create_timer(0.1).timeout
+		timeout += _delta
+		if timeout > 0.1:
 			player.visible_objects.erase(get_instance_id())
