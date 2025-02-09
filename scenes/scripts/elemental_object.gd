6b099c53cfb83f56928b4794ace90780aedd7982
Rokcas <rokas0090@gmail.com>
2025-02-09 12:03:14 +0000

    Make elemental objects check if they're visible on screen

--- scenes/scripts/elemental_object.gd
+++ scenes/scripts/elemental_object.gd
@@ -2,13 +2,14 @@ extends Node2D
 
 @export var element: Element.TYPE
 @onready var player = Global.player
+@export var visibility: VisibleOnScreenNotifier2D
 
 func _process(_delta: float) -> void:
 	var heart = player.get_node("Heart")
 	$RayCast2D.target_position = $RayCast2D.to_local(player.global_position)
 	$RayCast2D.add_exception(player)
 	var t = $RayCast2D.get_collider()
-	if t == heart:
+	if t == heart and visibility.is_on_screen():
 		player.visible_objects[get_instance_id()] = element
 	else:
 		player.visible_objects.erase(get_instance_id())
