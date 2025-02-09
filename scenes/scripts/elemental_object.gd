74d4c23f0664a4c659df991ac9b0b66fbfdbf8c1
jbachurski <kbachurski@gmail.com>
2025-02-09 17:00:44 +0000

    bunch of sprites

--- scenes/scripts/elemental_object.gd
+++ scenes/scripts/elemental_object.gd
@@ -4,11 +4,18 @@ extends Node2D
 @onready var player = Global.player
 @export var visibility: VisibleOnScreenNotifier2D
 var apply_timeout: float = 0
+var til_frame: float = 0.2
 
 func _ready() -> void:
 	$RayCast2D.add_exception(player)
 
 func _process(_delta: float) -> void:
+	til_frame -= _delta
+	if til_frame < 0:
+		til_frame = 0.2
+		var sprite = get_node_or_null("Sprite2D")
+		if sprite:
+			sprite.frame = (sprite.frame + 1) % (sprite.vframes * sprite.hframes)
 	var heart = player.get_node("Heart")
 	if visibility.is_on_screen():
 		$RayCast2D.target_position = $RayCast2D.to_local(player.global_position)
