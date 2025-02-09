32f2a4eab8c46896e1ca8cf0840671987b6b1ebd
spdskatr <25078279+spdskatr@users.noreply.github.com>
2025-02-09 12:19:43 +0000

    Tune level 1

--- scenes/scripts/level_1.gd
+++ scenes/scripts/level_1.gd
@@ -12,6 +12,16 @@ func post_spawn_dialogue():
 	await get_tree().create_timer(1).timeout
 	dialogue.queue_text("player", "It's dark in here...")
 	await dialogue.start_text()
+	await get_tree().create_timer(5).timeout
+	dialogue.queue_text("player", "Why is moving so difficult, all of a sudden?")
+	dialogue.queue_text("npc", "Are you looking at a boulder?")
+	dialogue.queue_text("player", "Um... yeah, I see a boulder here...")
+	dialogue.queue_text("npc", "Remember what I said about senses spilling over?")
+	dialogue.queue_text("npc", "Seeing a boulder causes you to feel heavy.")
+	dialogue.queue_text("npc", "When you're heavy, you cannot move. The only way to move again is to look away from the boulder.")
+	dialogue.queue_text("player", "Oh...")
+	dialogue.queue_text("npc", "Hold [SHIFT] to move without changing the direction you look.")
+	dialogue.start_text()
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -35,3 +45,19 @@ func _on_level_end_body_entered(body: Node2D) -> void:
 
 func next_scene():
 	get_tree().change_scene_to_file("res://scenes/levels/level2.tscn")
+
+
+var ice_tutorial_done = false
+func _on_ice_tut_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D and not ice_tutorial_done:
+		ice_tutorial_done = true
+		dialogue.queue_text("player", "That's an ice block...")
+		dialogue.queue_text("npc", "Ice blocks turn you into ice, which stops you from moving or changing direction.")
+		dialogue.queue_text("npc", "Ice is also slippery, so if you were moving before you turned into ice, you maintain your speed and slide along the ground.")
+		dialogue.queue_text("player", "What if I'm also heavy?")
+		dialogue.queue_text("npc", "If you become heavy while iced, you continue moving until you're no longer iced.")
+		dialogue.queue_text("player", "Huh.")
+		dialogue.queue_text("npc", "Oh, before I forget...")
+		dialogue.queue_text("npc", "If you find yourself stuck, you can press [R] or [F5] to restart the current puzzle.")
+		dialogue.start_text()
+		
