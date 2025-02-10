281596da8973104e9f448d860d6fd68fb953d6c3
Aaron Yeoh Cruz <zeyonaut@gmail.com>
2025-02-10 00:21:15 +0000

    Make fade-in slower

--- scenes/scripts/visible_world.gd
+++ scenes/scripts/visible_world.gd
@@ -15,7 +15,7 @@ func _process(_delta: float) -> void:
 		material.set_shader_parameter("brightness", interpolate(interpolate(ratio)))
 	else:
 		var weight = 1 - min(1, (Time.get_ticks_msec() - time_transition_initial) / TRANSITION_PERIOD)
-		material.set_shader_parameter("brightness", interpolate(interpolate(1 - weight * (1 - ratio))))
+		material.set_shader_parameter("brightness", 1 - weight * (1 - ratio))
 		
 func interpolate(x: float) -> float:
 	return x * (17. + x * -18. + x * x * 7.) / 6. 
